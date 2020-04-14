829b2609e0cf75acaa09460deb8b781eddaacd9c
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2020-04-14 23:07:49 +0900

    Udemyで作成したニュースアプリ、動作確認済み

---
+++ screens/HomeScreen.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import ListItem from '../components/ListItem';
+import Loading from '../components/Loading';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
+
+export default HomeScreen = ({ navigation }) => {
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        fetchArticles();
+    }, []);
+
+    const fetchArticles = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(URL);
+            setArticles(response.data.articles);
+        } catch (error) {
+            console.error(error);
+        }
+        setLoading(false);
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={articles}
+                renderItem={({ item }) => (
+                    <ListItem
+                        imageUrl={item.urlToImage}
+                        title={item.title}
+                        author={item.author}
+                        onPress={() =>
+                            navigation.navigate('Article', { article: item })
+                        }
+                    />
+                )}
+                keyExtractor={(item, index) => index.toString()}
+            />
+            {loading && <Loading />}
+        </SafeAreaView>
+    );
+};
